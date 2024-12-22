4c498ad873693eb17bd1c9756372351f73aaae57
Tinkermannn <wilmansaragih2@gmail.com>
2024-12-22 19:09:33 +0700

    First Commit

---
+++ Assets/TargetShooter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TargetShooter : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+
+    void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+            if(Physics.Raycast(ray, out RaycastHit hit)) {
+                Target target = hit.collider.gameObject.GetComponent<Target>();
+                
+                if(target != null) {
+                    target.Hit();
+                }
+            }
+        }
+    }
+}
