cd091093c45c3076092261f0e7dff654bf93df4f
Tinkermannn <wilmansaragih2@gmail.com>
2024-12-23 12:44:16 +0700

    Animation pas idle sama recoil

--- Assets/Scripts/TargetShooter.cs
+++ Assets/Scripts/TargetShooter.cs
@@ -6,11 +6,17 @@ using UnityEngine;
 public class TargetShooter : MonoBehaviour
 {
     [SerializeField] Camera cam;
-    public GameObject muzzlehEffect;
+    public GameObject muzzleEffect;
+    private Animator animator;
+    public Weapon weapon;
+
+    void Awake() {
+        animator = GetComponent<Animator>();
+    }
     void Update() {
         if(Input.GetMouseButtonDown(0)) {
-                    muzzlehEffect.GetComponent<ParticleSystem>().Play();
-
+            muzzleEffect.GetComponent<ParticleSystem>().Play();
+            weapon.Shoot(); // panggil disini
             Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
             if(Physics.Raycast(ray, out RaycastHit hit)) {
                 Target target = hit.collider.gameObject.GetComponent<Target>();
