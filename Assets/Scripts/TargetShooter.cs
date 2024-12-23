3fd12ea54a6138b9ed7f54ebbe1187845bbc0108
Tinkermannn <wilmansaragih2@gmail.com>
2024-12-23 11:56:42 +0700

    Add effect nembak sama namabhin map dikit

--- Assets/Scripts/TargetShooter.cs
+++ Assets/Scripts/TargetShooter.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class TargetShooter : MonoBehaviour
 {
     [SerializeField] Camera cam;
-
+    public GameObject muzzlehEffect;
     void Update() {
         if(Input.GetMouseButtonDown(0)) {
+                    muzzlehEffect.GetComponent<ParticleSystem>().Play();
+
             Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
             if(Physics.Raycast(ray, out RaycastHit hit)) {
                 Target target = hit.collider.gameObject.GetComponent<Target>();
