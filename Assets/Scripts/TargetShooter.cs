7edced8c299be48cee773bc7ad7f424068d6a480
Tinkermannn <wilmansaragih2@gmail.com>
2024-12-26 20:41:04 +0700

    Add Material Graffiti Enhanced kualitasi font Add Result, blm diintegrasikan

--- Assets/Scripts/TargetShooter.cs
+++ Assets/Scripts/TargetShooter.cs
@@ -1,30 +1,18 @@
 using System.Collections;
-<<<<<<< HEAD
-using UnityEngine;
-using UnityEngine.UI;
-=======
 using System.Data.Common;
 using UnityEditor.SearchService;
+using UnityEngine.UI;
 using UnityEngine;
->>>>>>> 2eded99efb4cb4bc2eac0f8a0b1c7c83036cae1d
 
 public class TargetShooter : MonoBehaviour
 {
     [SerializeField] private Camera cam;
     [SerializeField] private GameObject muzzleEffect;
     [SerializeField] private Weapon weapon;
-<<<<<<< HEAD
-    [SerializeField] private float gameDuration = 30f;
-
-    private Animator animator;
-    private float timer = 0f;
-    private bool isGameRunning = false;
-=======
 
     public static int shootCount = 0;
 
     private Animator animator; // Permainan dimulai dengan false
->>>>>>> 2eded99efb4cb4bc2eac0f8a0b1c7c83036cae1d
 
     void Awake()
     {
@@ -42,10 +30,21 @@ public class TargetShooter : MonoBehaviour
             Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
+                Debug.Log($"Hit object: {hit.collider.gameObject.name}");
+
+               Button button = hit.collider.GetComponentInParent<Button>() ?? hit.collider.GetComponentInChildren<Button>();
+                if (button != null)
+                {
+                    button.onClick.Invoke(); // Memanggil event onClick Button
+                    Debug.Log($"Button {button.name} clicked via shooting!");
+                }
+                else
+                {
                 Target target = hit.collider.GetComponent<Target>();
                 if (target != null)
                 {
                     target.Hit(); // Hancurkan target jika terkena tembakan
+                }
                 }
             }
         }
