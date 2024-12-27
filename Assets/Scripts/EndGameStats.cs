9cdc99fcf67a8718a99b451a5a349955ba01810d
idumpmysoul <abedzebua777@gmail.com>
2024-12-27 13:06:25 +0700

    Configuring Stats Data

---
+++ Assets/Scripts/EndGameStats.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EndGameStats : MonoBehaviour
+{
+    public static float accuracy = 0f;
+    public static int points = 0;
+    public static int totalTarget = 0;
+
+    public static int missed = 0;
+
+    public TextMeshProUGUI Score;
+    public TextMeshProUGUI TargetHit;
+    public TextMeshProUGUI ShotsFired;
+    public TextMeshProUGUI Accuracy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (points < 0)
+        {
+            points = 0;
+        }
+        else points = (TargetShooter.hitCount * 50) - ((FlickTargetSpawner.spawned - TargetShooter.hitCount) * 25) - 25;
+        totalTarget = FlickTargetSpawner.spawned;
+        if (totalTarget==0)
+        {
+            accuracy = TargetShooter.hitCount;
+        }
+        else accuracy = (TargetShooter.hitCount-1) * 100 / totalTarget;
+        Debug.Log("Accuracy : " + accuracy);
+        ShowAccuracy();
+        ShowPoints();
+        ShowHit();
+        ShowShots();
+    }
+
+    private void ShowAccuracy()
+    {
+        Accuracy.text = $"{accuracy}%";
+    }
+
+    private void ShowPoints()
+    {
+        Score.text = $"{points}";
+    }
+
+    private void ShowHit()
+    {
+        TargetHit.text = $"{TargetShooter.hitCount-1}/{totalTarget}";
+    }
+
+    private void ShowShots()
+    {
+        ShotsFired.text = $"{TargetShooter.shootCount}";
+    }
+
+
+
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,8 +5,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static bool flick = false;
-    public static int targetHit = 0;
-    public static int targetMissed = 0;
+
     void Update()
     {
         
